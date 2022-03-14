86947bc8b2d9d147e0a3554e5a1e20359fc930ce
vladleh9 <vladleh9@itlab-studio.com>
2022-03-15 00:18:46 +0200

    version 1.1 Add API/ Add ENDPOINTS

--- core/models.py
+++ core/models.py
@@ -1,9 +1,10 @@
 from core import db
 
 class ShortUrls(db.Model):
+    """Model Fields to create url"""
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(500), nullable=False)
     short_id = db.Column(db.String(20), nullable=False, unique=True)
-    temporary_url = db.Column(db.Integer(), nullable=False)
+    time_life = db.Column(db.Integer(), nullable=False)
     created_at = db.Column(db.Integer(), nullable=False)
     expiration_at = db.Column(db.Integer(), nullable=False)
--- core/routes.py
+++ core/routes.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import time
+from flask_restful import Resource, reqparse, abort
 from core.models import ShortUrls
 from core import app, db, api
 from random import choice
@@ -8,63 +8,69 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import InputRequired, NumberRange
 from flask import render_template, request, flash, redirect, url_for
-from flask import Flask, jsonify, request
+
+
+"""-----------------------APP FUNCTIONAL------------------------------------"""
 
 class UrlForm(FlaskForm):
-    """Class for WTF-FORM"""
+    """Entity to WTF-FORM"""
     url = StringField('url', validators=[InputRequired(message='The URL is required!')])
     custom_id = StringField('custom_id')
-    temporary_url = IntegerField('live_time', validators=[NumberRange(min=1, max=360, message='The URL is required!'), InputRequired(message='The URL is required!')])
+    time_life = IntegerField('time_life', validators=[NumberRange(min=1, max=360, message='Minimum input value 1, maximum input value 360'), InputRequired(message='The Number is required!')])
 
 def generate_short_id(num_of_chars: int):
-    """Function to generate random short_id  of specified number of characters"""
+    """Function to generate short_id of specified number of characters if custom_id is empty"""
     return ''.join(choice(string.ascii_letters+string.digits) for _ in range(num_of_chars))
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    """Function get data for form and generate short_id  """
+    """Function get data at form UrlForm add save in models ShortUrls"""
     form = UrlForm(request.form)
     if form.validate_on_submit():
         if request.method == 'POST':
-            url = request.form['url']
-            short_id = request.form['custom_id']
-            temporary_url = request.form['temporary_url']
 
-            if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
+            """Entity what field in form needs"""
+            url_form = request.form['url']
+            custom_id_form = request.form['custom_id']
+            time_life_form = request.form['time_life']
+
+            """Validation"""
+            if custom_id_form and ShortUrls.query.filter_by(short_id=custom_id_form).first() is not None:
                 flash('Please enter different custom id!')
                 return redirect(url_for('index'))
 
-            if not short_id:
-                short_id = generate_short_id(8)
+            if not custom_id_form:
+                custom_id_form = generate_short_id(3)
 
-            if not temporary_url:
-                temporary_url = 90
+            if not time_life_form:
+                time_life_form = 90
 
-            """Element to calculated time-life """
-            seconds = time.time() + (int(temporary_url) * 24 * 60 * 60)
+            """Calculations expiry date"""
+            days = time.time() + (int(time_life_form) * 24 * 60 * 60)
 
-            """Element to Save data to db """
-            new_link = ShortUrls(
-                original_url=url, short_id=short_id, temporary_url=temporary_url, created_at=time.time(), expiration_at=seconds)
+            """Save data in models ShortUrls"""
+            new_link = ShortUrls(original_url=url_form, short_id=custom_id_form, time_life=time_life_form, created_at=time.time(), expiration_at=days)
             try:
                 db.session.add(new_link)
                 db.session.commit()
                 return redirect('/')
             except:
-                return "ERROR:"
+                return "ERROR: Something wrong can't save data"
 
+    """Get all Urls in model ShortUrls and render templates"""
     short_urls = ShortUrls.query.all()
     return render_template('index.html', form=form, short_id=short_urls)
 
 @app.route('/<short_id>')
 def redirect_url(short_id):
-    """Function to redirected to origin url by short_id  """
+    """Function redirecting user to original link"""
     link = ShortUrls.query.filter_by(short_id=short_id).first()
+
+    """Validation"""
     if link:
-        """Element when time-life is expair delete this link """
         if link.expiration_at <= time.time():
-            flash('Invalid URL expiration_at TIME data was deleted')
+            """When the link lifetime is less than or equal to the current time, the link is removed"""
+            flash('The life-time URL has expired, the URL will be removed')
             db.session.delete(link)
             db.session.commit()
             return redirect('/')
@@ -73,3 +79,60 @@ def redirect_url(short_id):
     else:
         flash('Invalid URL')
         return redirect(url_for('index'))
+
+
+"""-----------------------API FUNCTION------------------------------------"""
+
+"""Creating entity Arguments"""
+urlpost_args = reqparse.RequestParser()
+urlpost_args.add_argument("original_url", type=str, help="The URL is required!", required=True)
+urlpost_args.add_argument("short_id", type=str, help="The ID is required!", required=True)
+urlpost_args.add_argument("time_life", type=int, help="The Time Life is required!", required=True)
+
+"""Api Get to Endpoint /list_urls"""
+class URL_list(Resource):
+    def get(self):
+        urls = ShortUrls.query.all()
+        data = {}
+        for url in urls:
+            data[url.id] = {"original_url": url.original_url, "short_id": "http://127.0.0.1:5000/" + url.short_id, url.time_life: "time_life"}
+        return data
+
+"""Api Post to Endpoint /create_urls"""
+class URL_create(Resource):
+    def post(self):
+        """Entity api arguments"""
+        args = urlpost_args.parse_args()
+        url_api = args['original_url']
+        short_id_api = args['short_id']
+        time_life_api = args['time_life']
+
+        """Validation"""
+        if short_id_api and ShortUrls.query.filter_by(short_id=short_id_api).first() is not None:
+            abort(409, message='Please enter different custom id!')
+
+        if not url_api:
+            abort(409, message='The URL is required!')
+
+        if not time_life_api:
+            time_life_api = 90
+
+        if not short_id_api:
+            short_id_api = generate_short_id(3)
+
+        """Calculations expiry date"""
+        days = time.time() + (int(time_life_api) * 24 * 60 * 60)
+
+        """Save data in models ShortUrls"""
+        new_link = ShortUrls(original_url=url_api, short_id=short_id_api, time_life=time_life_api, created_at=time.time(), expiration_at=days)
+        try:
+            db.session.add(new_link)
+            db.session.commit()
+            short_url = request.host_url + short_id_api
+            return short_url
+        except:
+            return "ERROR: Something wrong can't save data"
+
+""" Endpoints """
+api.add_resource(URL_create, '/create_urls')
+api.add_resource(URL_list, '/list_urls')
